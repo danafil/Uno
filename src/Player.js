c471ed3af9d5a35a080a344cd2e0d17aec5cbc40
DanaFil <danakleputs@gmail.com>
2018-03-28 23:57:52 +0200

    play in turns

--- src/Player.js
+++ src/Player.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 function Player(props) {
+  const className = props.active ? 'player-title--active' : 'player-title';
   return (
     <div>
-      <h2>{props.name}</h2>
+      <h2 className={className} >{props.name}</h2>
       <ul className="player-card">
         {props.cards.map((c, i) => (
           <li key={i} onClick={() => props.onPlay(c, props.name)}>
---
+++ src/winner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Winner(props) {
+  return (
+    <h1 className="gameOver">{props.name} win!</h1>
+  )
+}
